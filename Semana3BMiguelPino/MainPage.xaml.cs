39018e657da43d2c77b959d27b8761687b528773
Miguel <lucariodrago@gmail.com>
2021-10-22 21:06:26 -0500

    primer commit

---
+++ Semana3BMiguelPino/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana3BMiguelPino
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txt_usuario.Text;
+            string contrasena = txt_contraseña.Text;
+
+            await Navigation.PushAsync(new viewDos(usuario,contrasena));
+        }
+    }
+}
---
+++ Semana3BMiguelPino/viewDos.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana3BMiguelPino
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class viewDos : ContentPage
+    {
+        public viewDos(string usuario,string contrasena)
+        {
+            InitializeComponent();
+            //envio a los label
+            lbusuario.Text = usuario;
+            lbcontraseña.Text = contrasena;
+            //cajas de texto
+            txtUsuario.Text = usuario;
+            txtContrasena.Text = contrasena;
+        }
+
+        
+    }
+}
